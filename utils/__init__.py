3c4f02e6b810e79f61e64ddfc6cd162c8a2ac69c
luvneries <luvneries@gmail.com>
2018-12-05 09:04:00 -0600

    first commit

---
+++ utils/__init__.py
@@ -0,0 +1,5 @@
+# to call all functions available in utils folder by importing utils only
+from .misc import Plot
+from .data_manipulation import *
+from .data_operation import *
+from .kernels import *
